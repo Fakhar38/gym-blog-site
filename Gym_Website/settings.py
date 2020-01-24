1378337efa3566568cf7970d0178bc49859459ae
Fakhar Mahmood <fakharmahmood38@gmail.com>
2020-01-24 14:51:03 +0500

    added bootstrap/fonts

--- Gym_Website/settings.py
+++ Gym_Website/settings.py
@@ -22,7 +22,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p7b$qouekjq*@@rknvplt2d8q31qo=5&-ozzn9#iqe*fk&8*p3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
